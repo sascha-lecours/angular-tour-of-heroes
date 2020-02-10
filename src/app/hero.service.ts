49d252745e46e94956444476fee04703c214d769
slecours <sascha.lecours@gmail.com>
2020-02-10 18:49:39 -0500

    everything up to networking w/ server

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -11,8 +11,13 @@ export class HeroService {
 	
   constructor(private messageService: MessageService) { }
 
+  getHero(id: number): Observable<Hero>{
+	// TDOD: send the message _AFTER_ fetching the hero
+	this.messageService.add(`HeroService: fetched the hero id=${id}`);
+	return of(HEROES.find(hero => hero.id === id));
+  }
   getHeroes(): Observable<Hero[]> {
-	  // TDOD: send the message _AFTER_ fetching the heroes
+	// TDOD: send the message _AFTER_ fetching the heroes
 	this.messageService.add('HeroService: fetched heroes');
 	return of(HEROES);
   }
